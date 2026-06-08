0b41c94ab610d91e4c2413987731d1564b468d46
Shravani Patil <shravaniupatil4444@gmaiul.com>
2026-06-08 14:21:05 +0530

    Refactor SQLALCHEMY_DATABASE_URI to use environment variable for database connection

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'super-secret-key-12345')
-    
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Shravani%4030@127.0.0.1/storevault"
+  
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') 
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
